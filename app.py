4b9c67abff029d705f8cdc408be046fa87268b13
Stefan More <stefan.more@iaik.tugraz.at>
2022-07-27 17:54:01 +0200

    adapt authorization to use HTTP header

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 TOKEN_SECRET_KEY = "foobar"
+TOKEN_HEADER_NAME = "X-KRAKEN-TOKEN"
 TOKEN_MAX_AGE = 5
 
 uploads_dir = os.path.join(app.instance_path, 'uploads')
@@ -30,25 +31,30 @@ def hello_world():  # put application's code here
 def upload():
     app.logger.info('Uploading ({})'.format(request.method))
 
-    if request.method == 'POST':
-        if 'targettoken' not in request.args:
-            abort(400, 'No token parameter found in request.')
+    if TOKEN_HEADER_NAME not in request.headers:
+        abort(400, 'No {} header found in request.'.format(TOKEN_HEADER_NAME))
 
-        token = request.args.get('targettoken')
-        s = URLSafeTimedSerializer(TOKEN_SECRET_KEY)
-        try:
-            tokenData, ts = s.loads(token, max_age=TOKEN_MAX_AGE, return_timestamp=True)
-        except SignatureExpired as err:
-            abort(400, 'Token invalid: {}'.format(err))
+    authHeader = request.headers.get(TOKEN_HEADER_NAME)
+    s = URLSafeTimedSerializer(TOKEN_SECRET_KEY)
+    try:
+        tokenData, ts = s.loads(authHeader, max_age=TOKEN_MAX_AGE, return_timestamp=True)
+    except SignatureExpired as err:
+        abort(400, 'Token invalid: {}'.format(err))
+
+    if request.method == 'POST':
+        if 'type' not in request.args or 'id' not in request.args:
+            abort(400, 'No type and/or id GET parameter found in request.')
 
         if 'credential' not in request.files:
             abort(400, 'No credential file found in request.')
 
         credential = request.files['credential']
-        filename = secure_filename(tokenData)
-        storagePath = os.path.join(uploads_dir, filename)
+        filetype = secure_filename(request.args.get('type'))
+        filename = '{}.json'.format(secure_filename(request.args.get('id')))
+        os.makedirs(os.path.join(uploads_dir, filetype), exist_ok=True)
+        storagePath = os.path.join(uploads_dir, filetype, filename)
         if os.path.isfile(storagePath):
-            abort(406, 'Credential already exists: {}'.format(filename))
+            abort(406, 'Credential already exists: {}/{}'.format(filetype, filename))
 
         credential.save(storagePath)
 
