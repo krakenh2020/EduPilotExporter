713b2fab41de7bf2ec133cd6177ca6500b762166
Stefan More <stefan.more@iaik.tugraz.at>
2022-07-27 18:43:24 +0200

    simple upload test

---
+++ tests/upload1.py
@@ -0,0 +1,23 @@
+import requests
+from itsdangerous import TimestampSigner, TimedSerializer
+
+url = 'http://127.0.0.1:5000/upload'
+
+SECRET_KEY = "foobar"
+
+
+def upload(filename, type, id):
+    s = TimedSerializer(SECRET_KEY, signer=TimestampSigner)
+    token = s.dumps('KRAKEN')
+    print(token)
+
+    headers = {'X-KRAKEN-TOKEN': token}
+    parameters = {'id': id,
+                  'type': type}
+    files = {'credential': open(filename, 'r')}
+    r = requests.post(url, files=files, headers=headers, params=parameters)
+    print(r.text)
+
+
+if __name__ == '__main__':
+    upload('testfiles/test1.txt', 'grade', 'bsc')
